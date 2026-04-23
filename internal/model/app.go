769d4407f7298fa7e4df0e121f87923615eddf93
anyone <anyonego86@163.com>
2026-04-23 16:52:40 +0800

    将项目中的实例封装到了一个Application结构体中

--- internal/model/app.go
+++ internal/model/app.go
@@ -1,13 +1,16 @@
 package model
 
 import (
+	"shortURL/pkg/snowflake"
+
 	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 type App struct {
-	Logger *zap.Logger
-	DB     *gorm.DB
-	RDB    *redis.Client
+	Logger    *zap.Logger
+	DB        *gorm.DB
+	RDB       *redis.Client
+	SnowFlake *snowflake.SnowFlake
 }
