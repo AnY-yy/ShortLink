1b9aead0eb70b400c25880e8d727c5a4a1062cc5
anyone <anyonego86@163.com>
2026-04-23 16:53:21 +0800

    Redis缓存操作

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,7 +1,10 @@
 package cache
 
 import (
+	"context"
 	"shortURL/internal/bootstrap"
+	"shortURL/internal/model"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -15,4 +18,18 @@ func NewCache() *Cache {
 	return &Cache{
 		rdb: bootstrap.Application.RDB,
 	}
+}
+
+func (c *Cache) CreateURL(ctx context.Context, req *model.CreateURLRequest) error {
+	// 短链设置为key 长链设置为value
+	key := req.SelfShortUrl
+	value := req.LongURL
+	// 过期时间
+	expireTime := time.Hour * time.Duration(*req.ExpireTime) // 将过期时间转换为时间单位 int -> time.Duration
+	// 写入缓存
+	err := c.rdb.Set(ctx, key, value, expireTime).Err()
+	if err != nil {
+		return err
+	}
+	return nil
 }
